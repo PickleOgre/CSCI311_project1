d5868a7f13f60d17bc5abc92197bdc246cd1f4c1
PickleOgre <skelkor2@gmail.com>
2025-09-28 12:11:07 -0700

    Implement Insertion Sort and test function

--- project_1.cpp
+++ project_1.cpp
@@ -125,7 +125,7 @@ void testBubbleSort()
 
 int insertionSort(vector<int> &v)
 {
-    int i = 1;
+    unsigned int i = 1;
     while (i < v.size())
     {
         int j = i;
@@ -138,9 +138,78 @@ int insertionSort(vector<int> &v)
         }
         i++;
     }
+    return 1;
 }
+
+void testInsertionSort()
+{
+    // Create ten vectors of size 100
+    vector<int> v0(100);
+    vector<int> v1(100);
+    vector<int> v2(100);
+    vector<int> v3(100);
+    vector<int> v4(100);
+    vector<int> v5(100);
+    vector<int> v6(100);
+    vector<int> v7(100);
+    vector<int> v8(100);
+    vector<int> v9(100);
+
+    // Initialize RNG
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, 100);
+
+    // Fill vectors with random numbers
+    for (int i = 0; i < 100; i++)
+    {
+        v0[i] = dis(gen);
+        v1[i] = dis(gen);
+        v2[i] = dis(gen);
+        v3[i] = dis(gen);
+        v4[i] = dis(gen);
+        v5[i] = dis(gen);
+        v6[i] = dis(gen);
+        v7[i] = dis(gen);
+        v8[i] = dis(gen);
+        v9[i] = dis(gen);
+    }
+
+    // Test sorting algorithm
+    insertionSort(v0);
+    insertionSort(v1);
+    insertionSort(v2);
+    insertionSort(v3);
+    insertionSort(v4);
+    insertionSort(v5);
+    insertionSort(v6);
+    insertionSort(v7);
+    insertionSort(v8);
+    insertionSort(v9);
+
+    // Verify and print results
+    cout << "~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~" << endl;
+    cout << "Insertion Sort on 10 vectors of 100 elements." << endl;
+    if (insertionSort(v0) &&
+        insertionSort(v1) &&
+        insertionSort(v2) &&
+        insertionSort(v3) &&
+        insertionSort(v4) &&
+        insertionSort(v5) &&
+        insertionSort(v6) &&
+        insertionSort(v7) &&
+        insertionSort(v8) &&
+        insertionSort(v8) &&
+        insertionSort(v9))
+    {
+        cout << "Sorting successful." << endl;
+    }
+    cout << "~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~" << endl;
+}
+
 
 int main()
 {
     testBubbleSort();
+    testInsertionSort();
 }
